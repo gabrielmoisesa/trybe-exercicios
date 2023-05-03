dda7e662e0aa58b796f0d44da6293bae3c4631e8
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-05-03 19:38:01 -0300

    add ex-08.js

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-agora-a-pratica/ex-08.js
@@ -0,0 +1,9 @@
+ let number01 = 2;
+ let number02 = 6;
+ let number03 = 13;
+
+if (number01 % 2 === 0 || number02 % 2 === 0 || number03 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
