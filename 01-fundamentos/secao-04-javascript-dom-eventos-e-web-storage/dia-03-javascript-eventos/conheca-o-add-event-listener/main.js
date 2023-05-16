084b7f98ec09d1d97ff51dea958fe67685608355
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-05-15 23:59:40 -0300

    mk conheca-o-addEL exercises

--- 01-fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conheca-o-add-event-listener/main.js
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conheca-o-add-event-listener/main.js
@@ -8,19 +8,40 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele.
 // 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
+// R: Porque a classe 'tech' que está aplicada na primeira caixa tem uma alteração em sua posição no CSS.
+
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
-
+const addTechClass = (event) => {
+    const techClass = document.querySelector('.tech');
+    techClass.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+firstLi.addEventListener('click', addTechClass);
+secondLi.addEventListener('click', addTechClass);
+thirdLi.addEventListener('click', addTechClass);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
-
+input.addEventListener('input', (event) => {
+  const techClass = document.querySelector('.tech');
+  techClass.innerText = event.target.value;
+});
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
-
+myWebpage.addEventListener('dblclick', () => {
+  window.location.replace('https://blog.betrybe.com/');
+});
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
+myWebpage.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'green';
+});
 
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset';
+});
 // Segue abaixo um exemplo de uso do event.target.
 
 const resetText = (event) => {
