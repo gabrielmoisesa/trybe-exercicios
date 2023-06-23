1ecfb879ec77d1a53057e7decada1f1e1ae7fc3b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-06-22 23:31:43 -0300

    fix lint

--- 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
+++ 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
@@ -1,9 +1,9 @@
-import "./style.css"
-import validator from "validator";
+import './style.css';
+import validator from 'validator';
 
 const inputText = document.getElementById('inputText');
 const submitBtn = document.querySelector('button');
-const optSelected = document.querySelector('#option')
+const optSelected = document.querySelector('#option');
 const result = document.getElementById('result');
 
 submitBtn.addEventListener('click', () => {
@@ -13,7 +13,7 @@ submitBtn.addEventListener('click', () => {
         email: validator.isEmail(inputText.value),
         url: validator.isURL(inputText.value),
         lowercase: validator.isLowercase(inputText.value),
-        number: validator.isMobilePhone(inputText.value, "pt-BR")
+        number: validator.isMobilePhone(inputText.value, 'pt-BR')
     };
     result.innerHTML = `A validação retornou ${validators[optSelected.value]}`;
 });
