1576cbf1364819000954c20ab208e7b23b87c358
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-06-25 18:50:30 -0300

    add trybe eslint

--- 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-js-assincrono-promises-e-fetch/api-de-super-herois/src/main.js
+++ 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-js-assincrono-promises-e-fetch/api-de-super-herois/src/main.js
@@ -4,24 +4,24 @@ const img = document.querySelector('#image');
 const heroName = document.querySelector('#name');
 const btn = document.querySelector('#button');
 
-const BASE_URL = `https://akabab.github.io/superhero-api/api`;
+const BASE_URL = 'https://akabab.github.io/superhero-api/api';
 const MAX_HEROES = 1000;
 
 const randomId = () => Math.floor(Math.random() * MAX_HEROES);
 
 btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const id = randomId();
-    fetch(`${BASE_URL}/id/${id}.json`)
-        .then((result) => result.json())
-        .then((data) => {
-            img.src = data.images.md;
-            heroName.innerHTML = data.name;
-        })
-        .catch((error) => Swal.fire({
-            title: 'Hero not found',
-            text: error.message,
-            icon: 'error',
-            confirmButtonText: 'Cool',
-        }));
+  event.preventDefault();
+  const id = randomId();
+  fetch(`${BASE_URL}/id/${id}.json`)
+    .then((result) => result.json())
+    .then((data) => {
+      img.src = data.images.md;
+      heroName.innerHTML = data.name;
+    })
+    .catch((error) => Swal.fire({
+      title: 'Hero not found',
+      text: error.message,
+      icon: 'error',
+      confirmButtonText: 'Cool',
+    }));
 });
