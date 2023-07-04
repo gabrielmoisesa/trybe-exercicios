aeae9f861bd9684269132e24812f5339691b6feb
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-07-04 13:06:24 -0300

    create calculateAge function

--- 02-front-end/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-typescript/react-project-exercise/src/components/HelloWorld.tsx
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-typescript/react-project-exercise/src/components/HelloWorld.tsx
@@ -1,11 +1,39 @@
 import Title from "./Title";
 import ModuleDetails from "./ModuleDetails";
 
+const userInfo = {
+    firstName: "Gabriel",
+    lastName: "Alves",
+    birthDate: "18/08/2004",
+}
+
+function calculateAge(birthDate: string): number {
+    const currentDate = new Date();
+    const [day, month, year] = birthDate.split('/').map(Number);
+  
+    const userBirthDate = new Date(year, month - 1, day);
+    let age = currentDate.getFullYear() - userBirthDate.getFullYear();
+  
+    // Check if the birthday has already occurred this year
+    if (
+      currentDate.getMonth() < userBirthDate.getMonth() ||
+      (currentDate.getMonth() === userBirthDate.getMonth() &&
+        currentDate.getDate() < userBirthDate.getDate())
+    ) {
+      age--;
+    }
+  
+    return age;
+  }
+
 function HelloWorld() {
     return (
        <div>
            <Title />
            <ModuleDetails />
+           <p>
+            A idade do {userInfo.firstName} é {calculateAge(userInfo.birthDate)} anos.
+           </p>
        </div> 
     )
 }
