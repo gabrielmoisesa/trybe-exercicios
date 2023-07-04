719a435ae181dbe2ec8016da470a884184f0492b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-07-04 12:49:38 -0300

    add HelloWorld component

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-typescript/react-project-exercise/src/components/ModuleDetails.tsx
@@ -0,0 +1,12 @@
+function ModuleDetails() {
+    return (
+        <ol>
+            <li>Fundamentos</li>
+            <li>Front-end</li>
+            <li>Back-end</li>
+            <li>Ciência da Computação</li>
+        </ol>
+    )
+}
+
+export default ModuleDetails;
