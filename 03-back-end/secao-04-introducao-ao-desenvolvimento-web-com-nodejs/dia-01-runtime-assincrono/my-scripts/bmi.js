1e2cd47c6ac950964fffc41937958ee872af6833
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-06 19:27:57 -0300

    add to fixed in bmi & display bmi

--- 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-runtime-assincrono/my-scripts/bmi.js
+++ 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-runtime-assincrono/my-scripts/bmi.js
@@ -4,9 +4,13 @@ const calculateBMI = (weight, unit, height) => {
   const bmi = unit.toUpperCase() === 'M' 
   ? weight / height ** 2 
   : weight / (height / 100) ** 2;
-  return bmi;
+  
+  return bmi.toFixed(1);
 }
 
 const weight = readline.questionInt("What's your weight? In KG ")
 const heightUnit = readline.question("Height unit to use: M or CM? ")
 const height = readline.question(`What's your height? In ${heightUnit} `)
+
+const bmi = calculateBMI(weight, heightUnit, height)
+console.log(`Your BMI is ${bmi}`);
