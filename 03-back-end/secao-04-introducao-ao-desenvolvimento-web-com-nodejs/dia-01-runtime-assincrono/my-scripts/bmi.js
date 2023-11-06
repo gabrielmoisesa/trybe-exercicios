31157f7d5843ce9683ba95df546dcc981e115e44
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-06 19:19:41 -0300

    ex 3

--- 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-runtime-assincrono/my-scripts/bmi.js
+++ 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-runtime-assincrono/my-scripts/bmi.js
@@ -1,6 +1,12 @@
-const calculateBMI = (weight, height) => {
-  const bmi = weight / height ** 2;
+const readline = require('readline-sync')
+
+const calculateBMI = (weight, unit, height) => {
+  const bmi = unit.toUpperCase() === 'M' 
+  ? weight / height ** 2 
+  : weight / (height / 100) ** 2;
   return bmi;
 }
 
-console.log('bmi.js is loaded');
+const weight = readline.question("What's your weight? In KG ")
+const heightUnit = readline.question("Height unit to use: M or CM? ")
+const height = readline.question(`What's your height? In ${heightUnit} `)
