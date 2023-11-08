f97be58c5708b288acebdcf64195a3e2fea5c578
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-08 17:57:15 -0300

    create & respond teams array

--- 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Hello World!' }))
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Hello World!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 module.exports = app;
