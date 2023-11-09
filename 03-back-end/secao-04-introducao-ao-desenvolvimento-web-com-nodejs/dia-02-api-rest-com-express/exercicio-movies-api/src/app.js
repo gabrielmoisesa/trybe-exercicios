d12d1b5aed859a4b13c48cc9030649b9aab1af16
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-08 22:03:13 -0300

    ex 05

--- 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercicio-movies-api/src/app.js
+++ 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercicio-movies-api/src/app.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
+const { readMovies } = require("./fsUtils");
 
 const app = express();
 
-app.get('/', (_req, res) => res.status(200).json({ message: 'ok' }));
+app.get("/", (_req, res) => res.status(200).json({ message: "ok" }));
+
+app.get("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const movies = await readMovies();
+  const movie = movies.find((movie) => movie.id === Number(id));
+  if (!movie) return res.status(404).json({ message: "Movie not found" });
+  res.status(200).json(movie);
+});
 
 module.exports = app;
