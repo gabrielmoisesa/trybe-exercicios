9bc13ea9191addd616de668c860903a026cf7d78
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-08 22:08:22 -0300

    ex 06: create /movies get & use try catch in the gets

--- 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercicio-movies-api/src/app.js
+++ 03-back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercicio-movies-api/src/app.js
@@ -6,11 +6,24 @@ const app = express();
 app.get("/", (_req, res) => res.status(200).json({ message: "ok" }));
 
 app.get("/movies/:id", async (req, res) => {
-  const { id } = req.params;
-  const movies = await readMovies();
-  const movie = movies.find((movie) => movie.id === Number(id));
-  if (!movie) return res.status(404).json({ message: "Movie not found" });
-  res.status(200).json(movie);
+  try {
+    const { id } = req.params;
+    const movies = await readMovies();
+    const movie = movies.find((movie) => movie.id === Number(id));
+    if (!movie) return res.status(404).json({ message: "Movie not found" });
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/movies", async (_req, res) => {
+  try {
+    const movies = await readMovies();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = app;
