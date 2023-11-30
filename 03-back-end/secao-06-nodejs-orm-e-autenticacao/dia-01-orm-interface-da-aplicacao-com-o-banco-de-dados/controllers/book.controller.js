b917a2a12fc99bebb56594b15f220861b549445d
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-29 22:31:48 -0300

    ex 13: update book controller

--- 03-back-end/secao-06-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.controller.js
+++ 03-back-end/secao-06-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.controller.js
@@ -34,8 +34,25 @@ const create = async (req, res) => {
 
 }
 
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, pageQuantity } = req.body;
+    const book = await BookService.getById(id);
+    if (!book) return res.status(404).json({ message: 'Book not found!' });
+    if (!title || !author || !pageQuantity) {
+      return res.status(400).json({ message: 'Invalid data' });
+    }
+    const updatedBook = await BookService.update(id, { title, author, pageQuantity });
+    return res.status(200).json({ message: 'Book updated!'} );
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 };
