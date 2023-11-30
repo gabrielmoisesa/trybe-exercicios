f0dd033ab9d2c25fbb4dce0e7baf01cae5c39bf5
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-29 22:22:09 -0300

    ex 7: getbyid controller

--- 03-back-end/secao-06-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.controller.js
+++ 03-back-end/secao-06-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.controller.js
@@ -9,6 +9,18 @@ const getAll = async (_req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    return res.status(200).json(book);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };
