925c14a413455f0594b6b9cc0d4b78e9ef072978
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-19 16:57:53 -0300

    p-fixar: ex 1

---
+++ 03-back-end/secao-09-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicio-para-fixar/index.ts
@@ -0,0 +1,20 @@
+class Tv {
+  brand: string;
+  size: number;
+  resolution: string;
+  connections: string[];
+  connectedTo: string[];
+
+  constructor(
+    brand: string,
+    size: number,
+    resolution: string,
+    connections: string[]
+  ) {
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resolution;
+    this.connections = connections;
+    this.connectedTo = [];
+  }
+}
