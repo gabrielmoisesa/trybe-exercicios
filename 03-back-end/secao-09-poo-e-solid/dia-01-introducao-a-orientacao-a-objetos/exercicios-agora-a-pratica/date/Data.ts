e80b77673a748c8ebb20f3e30365bf17bc0b3203
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-20 17:41:07 -0300

    ex 5

---
+++ 03-back-end/secao-09-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios-agora-a-pratica/date/Data.ts
@@ -0,0 +1,11 @@
+class Data {
+  constructor(
+    private _day: number,
+    private _month: number,
+    private _year: number,
+  ) {
+    this._day = _day;
+    this._month = _month;
+    this._year = _year;
+  }
+}
