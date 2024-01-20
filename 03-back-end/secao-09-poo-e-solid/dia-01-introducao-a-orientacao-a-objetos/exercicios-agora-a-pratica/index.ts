2512bfd0349cd93c6a72d723211206316381c489
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-20 16:30:46 -0300

    ex 2

--- 03-back-end/secao-09-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios-agora-a-pratica/index.ts
+++ 03-back-end/secao-09-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios-agora-a-pratica/index.ts
@@ -10,4 +10,12 @@ class Student {
     this.testGrades = testGrades;
     this.schoolworkGrades = schoolworkGrades;
   }
+
+  totalScore() {
+    const testGradesSum = this.testGrades.reduce((acc, curr) => acc + curr);
+    const schoolworkGradesSum = this.schoolworkGrades.reduce((acc, curr) => acc + curr);
+    return testGradesSum + schoolworkGradesSum;
+  }
+
+  averageScore() {return this.totalScore() / 6;}
 }
