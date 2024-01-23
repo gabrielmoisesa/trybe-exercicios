19edffa572f97a45cc1c4e1d2c1849d6efe7e81f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-23 16:47:58 -0300

    p-fixar: ex 5

--- 03-back-end/secao-09-poo-e-solid/dia-03-polimorfismo/exercicios-para-fixar/game/Character.ts
+++ 03-back-end/secao-09-poo-e-solid/dia-03-polimorfismo/exercicios-para-fixar/game/Character.ts
@@ -1,4 +1,10 @@
 export default abstract class Character {
   abstract talk(): void;
+
   abstract specialMove(): void;
+
+  static introduce(character: Character) {
+    character.talk;
+    character.specialMove;
+  }
 }
